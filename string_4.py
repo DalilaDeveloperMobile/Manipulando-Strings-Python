eca5ea85a77975197d5f54b430e94150786c4c2a
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-26 11:00:32 -0300

    code: 4 String múltiplas linhas

---
+++ string_4.py
@@ -0,0 +1,37 @@
+nome = "Dalila"
+
+mensagem = f"""
+   Olá meu nome é {nome},
+ Eu estou aprendendo Python.
+      Essa mensagem tem diferentes recuos.  
+"""
+
+print(mensagem)
+
+# Result:
+
+#    Olá meu nome é Dalila,
+# Eu estou aprendendo Python.
+#      Essa mensagem tem diferentes recuos. 
+
+print(
+    """
+       ============== MENU ==============
+
+       1 - Depositar
+       2 - Sacar
+       0 - Sair
+
+       ==================================
+"""
+)
+
+# Result 
+
+ #      ============== MENU ==============
+
+ #      1 - Depositar
+ #      2 - Sacar
+ #      0 - Sair
+
+ #      ==================================
