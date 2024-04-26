fa4295f9c08f349f9785f26987204d0bc6b98df3
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-26 10:59:57 -0300

    code: 3 Fatiamento de string

---
+++ string_3.py
@@ -0,0 +1,25 @@
+nome = "Guilherme Arthur de Carvalho"
+
+print(nome[0])
+# G
+
+print(nome[-2])
+# h
+
+print(nome[:9])
+# Guilherme
+
+print(nome[10:])
+# Arthur de Carvalho
+
+print(nome[10:16])
+# Arthur
+
+print(nome[10:16:2])
+# Atu
+
+print(nome[:])
+# Guilherme Arthur de Carvalho
+
+print(nome[::-1])
+# ohlavraC ed ruhtrA emrehliuG
